d85ff20ed7e221c60dae3aecbee678565d3b0af8
Timofei Kovalev <timkovalev12@gmail.com>
2025-10-07 19:55:02 +0300

    deleted a debug page

--- app/check/page.tsx
+++
@@ -1,7 +0,0 @@
-import pool from "@/lib/db";
-
-export async function GET() {
-  const result = await pool.query("SELECT NOW()");
-  console.log(result.rows);
-  return new Response(JSON.stringify(result.rows));
-}
